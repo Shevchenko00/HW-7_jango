04434f3562f3170aad0a10f813fc756a59eda554
Shevchenko00 <shevchenko.sasha2005@gmail.com>
2024-09-10 21:35:24 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -51,9 +51,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-DATABASES = {
-    'default': env.db(),  # Читает переменную DATABASE_URL
-}
+if env.bool('MYSQL', default=False):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': env('DB_NAME'),
+            'USER': env('DB_USER'),
+            'PASSWORD': env('DB_PASSWORD'),
+            'HOST': env('DB_HOST', default='localhost'),
+            'PORT': env('DB_PORT', default='3306'),
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
